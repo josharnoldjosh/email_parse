54940ed7289333e0eac47ffd397cc04c0ef9f76b
Josh Arnold <josharnold997@gmail.com>
2018-05-17 23:28:33 -0700

    Added clear email and name outputs for the CSV

--- read_mail.py
+++ read_mail.py
@@ -1,3 +1,44 @@
+# modify script to save to csv format that at least includes the name, email and HackDavis contact email, HackDavis contact name
+import re
+
+def get_names(text):
+	names = []
+	i = 0
+	if ('<' in text):
+		new_text = re.sub('<[^>]+>', '', text)
+		new_text = new_text.strip()		
+		if (i > 0):
+			new_text += ','
+		i += 1	
+		if ('@' not in new_text):
+			names.append(new_text)
+	return names
+
+def get_email_string(text):
+	emails = []
+	if ('<' in text):
+		while '<' in text:
+			for i in range(0, len(text)):
+				if text[i] == '<':
+					text = text[i+1:]
+
+					for j in range(0, len(text)):
+						
+						if text[j] == '>':
+							text = text[:j]
+							emails.append(text)
+							break
+					break
+	elif(' ' not in text.strip()):
+		emails.append(text) 
+	elif(',' in text):
+		pattern = re.compile("^\s+|\s*,\s*|\s+$")
+		temp_arr = [x for x in pattern.split(text) if x]		
+		for i in range(0, len(temp_arr)-1):
+			temp_arr[i] = temp_arr[i] + ','
+		emails += temp_arr
+	return emails
+
 import mailbox, sys, csv
 
 import argparse
@@ -18,16 +59,22 @@ else:
 
 to_list = ["To (Person)"]
 subject_list = ["Email subject"]
+emails = ["To person\'s email"]
+names = ['To person\'s name']
 
 for message in mbox:
     if (message.is_multipart()):
-    	pay_load = message.get_payload()
-    	if (len(pay_load) >= args.num_in_thread):
-    		to_str = str(message['to'])
-    		subj_str = str(message['subject'])
-    		print(to_str, "\n", subj_str, '\n\n')    		    	    		    		
-    		to_list.append(to_str)
-    		subject_list.append(subj_str)
+        pay_load = message.get_payload()
+        if (len(pay_load) >= args.num_in_thread):
+            to_str = str(message['to'])
+
+            emails.append(get_email_string(to_str))
+            names.append(get_names(to_str))         
+
+            subj_str = str(message['subject'])
+            print(to_str, "\n", subj_str, '\n\n')
+            to_list.append(to_str)
+            subject_list.append(subj_str)
 
 should_save = input("Do you want to save to a CSV? y/n: ")
 if should_save == '':
@@ -43,7 +90,16 @@ if ('y' in should_save.lower()):
 	    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 	    i = 0
 	    while i < len(to_list):
-	    	wr.writerow([to_list[i], subject_list[i]])
+	    	to_write = [to_list[i], subject_list[i]]
+	    	if (i < len(names)):
+	    		if (i == 0):
+	    			to_write.append(names[i])
+	    		to_write.append(" ".join(names[i]))
+	    	if (i < len(emails)):
+	    		if (i == 0):
+	    			to_write.append(emails[i])
+	    		to_write.append(" ".join(emails[i]))	    	
+	    	wr.writerow(to_write)
 	    	i += 1
 	    print("Done!")
 
